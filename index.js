072b8356b12dd6f18d3984a347219da6f0654404
rafia02 <rafiabinterashed01@gmail.com>
2023-03-08 19:57:16 +0600

    all cart

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express= require('express')
-const app= express()
+const express = require('express')
+const app = express()
 const cors = require('cors')
 const port = process.env.PORT | 5000;
 
@@ -16,42 +16,74 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run (){
-    try{
-        const allCatagorisCollection= client.db("foodDeliveryDatabase").collection("allCatagoris")
-        const allProductsCollection= client.db("foodDeliveryDatabase").collection("allProducts")
+async function run() {
+    try {
+        const allCatagorisCollection = client.db("foodDeliveryDatabase").collection("allCatagoris")
+        const allProductsCollection = client.db("foodDeliveryDatabase").collection("allProducts")
+        const addToCartCollection = client.db("foodDeliveryDatabase").collection("allAddToCart")
 
 
-        app.get('/allCatagoris', async (req, res ) => {
-            const filter= { }
-            const result= await allCatagorisCollection.find(filter).toArray()
-            res.send(result)
-        })
-
-        app.get('/catagorisName', async (req, res)=> {
+        app.get('/allCatagoris', async (req, res) => {
             const filter = {}
-            const result= await allCatagorisCollection.find(filter).project({title: 1 } ).toArray()
+            const result = await allCatagorisCollection.find(filter).toArray()
+            res.send(result)
+        })
+
+        app.get('/catagorisName', async (req, res) => {
+            const filter = {}
+            const result = await allCatagorisCollection.find(filter).project({ title: 1 }).toArray()
             res.send(result)
         })
 
 
-        app.get('/itemsProducts/:title', async (req, res ) => {
+        app.get('/itemsProducts/:title', async (req, res) => {
             const title = req.params.title;
-            const filter = {catagoris: title }
+            const filter = { catagoris: title }
             const result = await allProductsCollection.find(filter).toArray()
             res.send(result)
         })
 
 
-        app.get('/allfood', async(req, res)=>{
+        app.get('/allfood', async (req, res) => {
             const query = {}
             const result = await allProductsCollection.find(query).toArray()
             res.send(result)
         })
 
 
-        
-    }finally{
+        // app.post('/allcart', async(req, res)=>{
+        //     const cartProduct = req.body 
+        //     const query = {productId: cartProduct.productId}
+
+        //     const isCart = await addToCartCollection.findOne(query)
+        //     console.log("query:", query, "isCart:", isCart)
+        //     // if(isCart){
+        //     //    const oldQuentity = req.body.quantity
+
+        //     //    const newQuantity: oldQuentity + 1
+        //     // }
+
+
+        //         const result = await addToCartCollection.insertOne(cartProduct)
+        //     res.send(result)
+        //     console.log(result)
+
+        // })
+
+
+
+        app.post('/allcart', async (req, res) => {
+            const cartProduct = req.body
+            const result = await addToCartCollection.insertOne(cartProduct)
+            res.send(result)
+            console.log(result)
+
+        })
+
+
+
+
+    } finally {
 
     }
 }
@@ -61,10 +93,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', async (req, res)=>{
-     res.send("Food Server is run client")
+app.get('/', async (req, res) => {
+    res.send("Food Server is run client")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Food server run ${port}`)
 })
